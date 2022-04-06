3c6ac12539a47f0db0faa0dcc93e6c5bc6f6225d
vuke123 <lv53072@fer.hr>
2022-04-06 10:55:32 +0200

    CustomCells w/ use of storyboard for constraints and linking CustomTableViewCell
    Co-Authored-By: AntePrpic <58590535+AntePrpic@users.noreply.github.com>

---
+++ SofaAcademyMarch4/SofaAcademyMarch4/CustomTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomTableViewCell.swift
+//  SofaAcademyMarch4
+//
+//  Created by Luka Vukelić on 06.04.2022..
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+
+
+}
---
+++ SofaAcademyMarch4/SofaAcademyMarch4/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  SofaAcademyMarch4
+//
+//  Created by Luka Vukelić on 06.04.2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+
+    @IBOutlet weak var table: UITableView!
+//    interface builder
+    
+    struct App {
+        let title: String
+        let imageName: String
+    }
+    let data : [App] = [
+        App(title:"facebook", imageName: "facebook"),
+        App(title:"instagram", imageName: "instagram"),
+        App(title:"snapchat", imageName: "snap"),
+        App(title:"tiktok", imageName: "tiktok"),
+        App(title:"youtube", imageName: "yt")
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        table.dataSource = self
+        table.delegate = self
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let app = data[indexPath.row]
+        let cell = table.dequeueReusableCell(withIdentifier: "cell", for:indexPath) as! CustomTableViewCell
+        cell.label.text = app.title
+        cell.label.textColor = .systemIndigo
+        cell.label.isHighlighted = true
+        cell.label.shadowColor = .systemGray2
+        NSLayoutConstraint.activate([
+        cell.label.widthAnchor.constraint(equalToConstant: 60)
+        ])
+        cell.iconImageView.image = UIImage(named: app.imageName)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
+    }
+    
+}
+
